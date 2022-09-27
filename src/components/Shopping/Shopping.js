474c7c8649a99b418738d417a45743240b74a981
islammd39e <islammdrahat308@gmail.comm>
2022-09-27 21:21:37 +0600

    calculation cart detailes

--- src/components/Shopping/Shopping.js
+++ src/components/Shopping/Shopping.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Shopping.css'
 
-const Shopping = ({cart}) => {
+const Shopping = (props) => {
+    const {cart}=props;
+    let price = 0;
+    let shipping = 0;
+    for(const product of cart){
+        price = price + product.price
+        shipping = shipping + product.shipping
+    }
+    const tax = (price * 0.1).toFixed(2)
     return (
         <div className='carts'>
             <h3>Order Summary</h3>
             <p>Your items : {cart.length}</p>
+            <p>Total Price : ${price}</p>
+            <p>Total Shipping : ${shipping}</p>
+            <p>Tax : ${tax}</p>
+            <h3>Grand Total :</h3>
         </div>
     );
 };
