492aca70dca72b2aba0b183647c7186c3ed7e92b
islammd39e <islammdrahat308@gmail.comm>
2022-09-27 20:18:32 +0600

    upload test

--- src/components/Shopping/Shopping.js
+++ src/components/Shopping/Shopping.js
@@ -5,7 +5,7 @@ const Shopping = ({cart}) => {
     return (
         <div className='carts'>
             <h3>Order Summary</h3>
-                <p>Your items : {cart.length}</p>
+            <p>Your items : {cart.length}</p>
         </div>
     );
 };
